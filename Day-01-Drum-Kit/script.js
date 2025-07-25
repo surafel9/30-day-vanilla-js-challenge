d45a672ba02a7e9d2c9ad460c8f4c16763bcafa7
Surafel Fisseha <surafel.fiss@gmail.com>
2025-07-25 22:02:55 +0300

    update

--- Day-01-Drum-Kit/script.js
+++ Day-01-Drum-Kit/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+window.addEventListener('keydown', function (e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+});
