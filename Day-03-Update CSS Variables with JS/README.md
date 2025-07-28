a5e6e4aafc1cd445f0c285325988b8df3c67b2d1
Surafel Fisseha <surafel.fiss@gmail.com>
2025-07-28 18:23:18 +0300

    update

---
+++ Day-03-Update CSS Variables with JS/README.md
@@ -0,0 +1,5 @@
+# 🎛️ CSS Variables & JavaScript
+
+A simple UI that updates **CSS variables** like spacing, blur, and base color in real time using JavaScript.
+
+![Screenshot](./Screenshot%20From%202025-07-28%2018-19-15.png)
---
+++ Day-03-Update CSS Variables with JS/script.js
@@ -0,0 +1,13 @@
+'use strict';
+const input = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+input.forEach(input => input.addEventListener('change', handleUpdate));
+input.forEach(input => input.addEventListener('mousemove', handleUpdate));
