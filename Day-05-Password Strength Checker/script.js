6db2f29e64794a128a6fb42e2f84058756011901
Surafel Fisseha <surafel.fiss@gmail.com>
2025-07-30 16:47:32 +0300

    Update All

--- Day-05-Password Strength Checker/script.js
+++ Day-05-Password Strength Checker/script.js
@@ -1,20 +1,48 @@
 'Use strict';
-const inputText = document.querySelector('#password-input');
-const strengthText = document.querySelector('.strength-text');
+const inputText = document.querySelector('#password');
+const strengthText = document.querySelector('#strength-text');
+const togglePassword = document.querySelector('#togglePassword');
 
-// console.log(inputText.value);
+const requirements = {
+  length: document.querySelector(`li[data-requirement="length"]`),
+  case: document.querySelector(`li[data-requirement="case"]`),
+  number: document.querySelector(`li[data-requirement="number"]`),
+  symbol: document.querySelector(`li[data-requirement="symbol"]`),
+};
 
-inputText.addEventListener('input', function (event) {
-  const password = event.target.value;
-  let strength = {
-    level: 'none',
-    score: 0,
-  };
-  if (password.length > 0) {
-    let score = 0;
-    if (password.length >= 8) score++;
-    if ()
+//
+inputText.addEventListener('input', event => {
+  const pass = event.target.value;
+  requirements.length.classList.toggle('valid', pass.length > 7);
+  requirements.case.classList.toggle(
+    'valid',
+    /[a-z]/.test(pass) && /[A-Z]/.test(pass)
+  );
+  requirements.number.classList.toggle('valid', /[0-9]/.test(pass));
+  requirements.symbol.classList.toggle('valid', /[^A-Za-z0-9]/.test(pass));
+  let strength = 'Weak';
+  let color = 'red';
+  if (
+    pass.length > 7 &&
+    /[A-Z]/.test(pass) &&
+    /\d/.test(pass) &&
+    /[@$!%*?&#]/.test(pass)
+  ) {
+    strength = 'Strong';
+    color = 'green';
+  } else if (pass.length > 7 && /\d/.test(pass)) {
+    strength = 'Medium';
+    color = 'orange';
   }
+
+  strengthText.textContent = strength;
+  strengthText.style.color = color;
+  console.log(pass);
 });
 
-// strength-text
+togglePassword.addEventListener('click', function () {
+  const type =
+    inputText.getAttribute('type') === 'password' ? 'text' : 'password';
+  inputText.setAttribute('type', type);
+  this.textContent = type === 'password' ? '👁️' : '🙈';
+});
