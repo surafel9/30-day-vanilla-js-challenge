66446981aa1395472c404d5cb5ffa90fbc396739
Surafel Fisseha <surafel.fiss@gmail.com>
2025-07-31 16:59:38 +0300

    update js

--- Day-06-Countdown Timer/script.js
+++ Day-06-Countdown Timer/script.js
@@ -12,5 +12,32 @@ function startTimer() {
   const days = parseInt(daysInput.value) || 0;
   const hours = parseInt(hourInput.value) || 0;
   const minutes = parseInt(minuteInput.value) || 0;
-  const second = parseInt(secondInput.value) || 0;
+  const seconds = parseInt(secondInput.value) || 0;
+
+  let totalSeconds = days * 86400 + hours * 3600 + minutes * 60 + seconds;
+
+  if (totalSeconds <= 0) {
+    return;
+  }
+  clearInterval(countdown);
+
+  countdown = setInterval(() => {
+    if (totalSeconds <= 0) {
+      clearInterval(countdown);
+      heading.textContent = "Time's Up!";
+      return;
+    }
+    totalSeconds--;
+
+    const d = Math.floor(totalSeconds / 86400);
+    const h = Math.floor((totalSeconds % 86400) / 3600);
+    const m = Math.floor((totalSeconds % 3600) / 60);
+    const s = totalSeconds % 60;
+
+    daysInput.value = d;
+    hourInput.value = h;
+    minuteInput.value = m;
+    secondInput.value = s;
+  }, 1000);
 }
+startButton.addEventListener('click', startTimer);
