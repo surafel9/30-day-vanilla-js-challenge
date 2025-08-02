426c7e8db40933fe342f77bd320a918fa3603154
Surafel Fisseha <surafel.fiss@gmail.com>
2025-08-02 23:58:29 +0300

    update

--- Day-07- Music Player/script.js
+++ Day-07- Music Player/script.js
@@ -2,31 +2,49 @@
 
 const songs = [
   {
-    title: 'Cry',
+    title: '♪ Cry',
     artist: 'Cigarettes After Sex',
     src: 'songs/Cigarettes-After-Sex-Cry-Lyrics.m4a',
   },
   {
-    title: 'The Night We mate',
+    title: '♪ The Night We mate',
     artist: 'Lord Huron',
     src: 'songs/Lord-Huron-The-Night-We-Met-Official-Lyric-Video.m4a',
   },
   {
-    title: 'Chezila',
+    title: '♪ Chezila',
     artist: 'Beanie',
     src: 'songs/Chezile-Beanie-Lyrics.m4a',
   },
   {
-    title: 'Beach House',
-    artist: 'Space Song',
+    title: '♪ Space Song',
+    artist: 'Beach House',
     src: 'songs/Chezile-Beanie-Lyrics.m4a',
   },
 ];
+const randome = Math.floor(Math.random() * songs.length);
+document.querySelector('.artist').textContent = songs[randome].artist;
+document.querySelector('.title').textContent = songs[randome].title;
 
-let currentSongIndex = 0;
+const artistName = document.querySelector('.artist');
+const songTitle = document.querySelector('.title');
 const audio = document.getElementById('my-songs');
-const playBtn = document.getElementById('play-pause-btn');
+const playPauseBtn = document.getElementById('play-pause-btn');
 const prevBtn = document.getElementById('rewind-btn');
 const nextBtn = document.getElementById('forward-btn');
+const playPauseIcon = playPauseBtn.querySelector('i');
 
-loadSong(songs[currentSongIndex]);
+let currentSongIndex = 0;
+let isPlaying = false;
+
+function loadsong(song) {
+  songTitle.textContent = song.title;
+  artistName.textContent = song.artist;
+  audio.src = song.src;
+}
+
+function playSong() {
+  isPlaying = true;
+  audio.play();
+  playPauseIcon.classList.replace('fa-play', 'fa-pause');
+}
