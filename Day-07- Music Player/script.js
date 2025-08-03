da4df63d5cfb681a56a630b5e58400a8961af707
Surafel Fisseha <surafel.fiss@gmail.com>
2025-08-03 20:47:57 +0300

    update

--- Day-07- Music Player/script.js
+++ Day-07- Music Player/script.js
@@ -3,22 +3,22 @@
 const songs = [
   {
     title: '♪ Cry',
-    artist: 'Cigarettes After Sex',
+    artist: '🎙️Cigarettes After Sex',
     src: 'songs/Cigarettes-After-Sex-Cry-Lyrics.m4a',
   },
   {
     title: '♪ The Night We mate',
-    artist: 'Lord Huron',
+    artist: '🎙️Lord Huron',
     src: 'songs/Lord-Huron-The-Night-We-Met-Official-Lyric-Video.m4a',
   },
   {
     title: '♪ Chezila',
-    artist: 'Beanie',
+    artist: '🎙️Beanie',
     src: 'songs/Chezile-Beanie-Lyrics.m4a',
   },
   {
     title: '♪ Space Song',
-    artist: 'Beach House',
+    artist: '🎙️Beach House',
     src: 'songs/Chezile-Beanie-Lyrics.m4a',
   },
 ];
@@ -34,10 +34,12 @@ const prevBtn = document.getElementById('rewind-btn');
 const nextBtn = document.getElementById('forward-btn');
 const playPauseIcon = playPauseBtn.querySelector('i');
 
-let currentSongIndex = 0;
+let currentSongIndex = Math.floor(Math.random() * songs.length);
+loadSong(songs[currentSongIndex]);
+
 let isPlaying = false;
 
-function loadsong(song) {
+function loadSong(song) {
   songTitle.textContent = song.title;
   artistName.textContent = song.artist;
   audio.src = song.src;
@@ -48,3 +50,34 @@ function playSong() {
   audio.play();
   playPauseIcon.classList.replace('fa-play', 'fa-pause');
 }
+function pauseSong() {
+  isPlaying = false;
+  audio.pause();
+  playPauseIcon.classList.replace('fa-pause', 'fa-play');
+}
+function prevSong() {
+  currentSongIndex--;
+  if (currentSongIndex < 0) {
+    currentSongIndex = songs.length - 1;
+  }
+  loadSong(songs[currentSongIndex]);
+  playSong();
+}
+function nextSong() {
+  currentSongIndex++;
+  if (currentSongIndex > songs.length - 1) {
+    currentSongIndex = 0;
+  }
+  loadSong(songs[currentSongIndex]);
+  playSong();
+}
+
+playPauseBtn.addEventListener('click', () =>
+  isPlaying ? pauseSong() : playSong()
+);
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('ended', nextSong);
+
+loadSong(songs[currentSongIndex]);
