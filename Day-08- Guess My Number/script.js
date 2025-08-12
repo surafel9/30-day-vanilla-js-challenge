5fdb3894ca274ef0d97c4a9021c4fd07e6e22261
Surafel Fisseha <surafel.fiss@gmail.com>
2025-08-12 03:28:53 +0300

    update

--- Day-08- Guess My Number/script.js
+++ Day-08- Guess My Number/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let guessedNumber = document.querySelector('#guess-number');
+const checkButton = document.querySelector('.btn.check');
+const reset = document.querySelector('.btn.again');
+
+// guessedNumber = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+guessedNumber = randomNumber;
+// console.log(guessedNumber);
